4182750e8380df17a9c6044bb49f693d49b10786
hessaalmusafer <hessaalmusafer@gmail.com>
2022-09-11 00:08:25 +0300

    done

--- CW3B/CW3B/ContentView.swift
+++ CW3B/CW3B/ContentView.swift
@@ -7,10 +7,86 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View
+{
+    @State var counter1 = 0
+    @State var counter2 = 0
+    @State var counter3 = 0
+
+    var body: some View
+    {
+        VStack
+        {
+            HStack
+            {
+            Text("استغفر الله العظيم")
+                    .foregroundColor(.black)
+                    .bold()
+                    .font(.largeTitle)
+                ZStack
+                {
+                Circle()
+                    .frame(width: 110, height: 110)
+                    .foregroundColor(.brown)
+                    
+                    Text("\(counter1)")
+                        .bold()
+                        .font(.largeTitle)
+                }
+                .onTapGesture
+                {
+                    counter1 = counter1 + 1
+                }
+            }
+            
+            
+                HStack
+                {
+                Text("سبحان الله وبحمده")
+                        .foregroundColor(.black)
+                        .bold()
+                        .font(.largeTitle)
+                    ZStack
+                    {
+                    Circle()
+                        .frame(width: 110, height: 110)
+                        .foregroundColor(.brown)
+                        
+                        Text("\(counter2)")
+                            .bold()
+                            .font(.largeTitle)
+                    }
+                    .onTapGesture
+                    {
+                        counter2 = counter2 + 1
+                    }
+
+                }
+            
+            
+                HStack
+                {
+                Text("سبحان الله العظيم")
+                        .foregroundColor(.black)
+                        .bold()
+                        .font(.largeTitle)
+                    ZStack
+                    {
+                    Circle()
+                        .frame(width: 110, height: 110)
+                        .foregroundColor(.brown)
+                        
+                        Text("\(counter3)")
+                            .bold()
+                            .font(.largeTitle)
+                    }
+                    .onTapGesture
+                    {
+                        counter3 = counter3 + 1
+                    }
+
+                }
+        }
     }
 }
 
