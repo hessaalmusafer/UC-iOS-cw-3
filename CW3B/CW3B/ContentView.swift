9b0ff57a0539d43da84f2bc48c51eea3befa816f
hessaalmusafer <hessaalmusafer@gmail.com>
2022-09-10 19:50:09 +0300

    /

--- CW3B/CW3B/ContentView.swift
+++ CW3B/CW3B/ContentView.swift
@@ -7,95 +7,10 @@
 
 import SwiftUI
 
-struct MyOtherApp: View {
-    @State var grade = ""
-    @State var result = ""
-
-    var body: some View {
-        
-                VStack
-                {
-                Text("Grade Calculator")
-                    .bold()
-                    .font(.largeTitle)
-                    
-                    
-                    Image("calc")
-                        .resizable()
-                        .scaledToFit()
-                        .padding()
-                    
-                    TextField("enter your grade" , text: $grade)
-                        .font(.largeTitle)
-        
-                    
-                    ZStack
-                    {
-                        Color(.gray)
-                        Text("Calculate")
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                    
-                    .frame(width: 100, height: 40)
-                    .onTapGesture {
-                        if(Double(grade) ?? 0 >= 90.0)
-                        {
-                        Text("A")
-                                .bold()
-                                .font(.largeTitle)
-                        }
-                        
-                        else if (Double(grade) ?? 0 >= 80.0)
-                        {
-                            Text("B")
-                                    .bold()
-                                    .font(.largeTitle)
-                        }
-                        
-                        else if (Double(grade) ?? 0 >= 70.0)
-                        {
-                            Text("C")
-                                    .bold()
-                                    .font(.largeTitle)
-                        }
-                        
-                        else if (Double(grade) ?? 0 >= 60.0)
-                        {
-                            Text("D")
-                                    .bold()
-                                    .font(.largeTitle)
-                        }
-                        
-                        else if (Double(grade) ?? 0 < 60.0)
-                        {
-                            result = "F"
-                        }
-                    }
-
-                    
-                    Text("Your Grade is:")
-                        .bold()
-                            
-                    Text(result)
-                            .bold()
-                            .font(.largeTitle)
-
-                            
-
-                }
-            
-    }
-}
-
-
 struct ContentView: View {
     var body: some View {
-        HStack {
-            MyOtherApp()
-            MyOtherApp()
-            MyOtherApp()
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
